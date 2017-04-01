0ab2372a0ffcdec830521ce8874bf40cb8819401
Chase Zheng <mzheng086@gmail.com>
2017-03-31 23:07:38 -0700

    main activity layout

--- main/java/me/connectedspace/accelerometer/MainActivity.java
+++ main/java/me/connectedspace/accelerometer/MainActivity.java
@@ -1,5 +1,9 @@
 package me.connectedspace.accelerometer;
 
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -8,27 +12,29 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.widget.TextView;
+import android.support.v7.widget.AppCompatButton;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    TextView x;
-    TextView y;
-    TextView z;
+    TextView currentTime = (TextView) findViewById (R.id.currentTime);
+    TextView currentAccel = (TextView) findViewById (R.id.currentAccel);
+    Context mContext = getApplicationContext();
+    Intent serviceIntent = new Intent(mContext, Service.class);
+    PendingIntent servicePendingIntent = PendingIntent.getActivity(mContext, 1, serviceIntent);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        x = (TextView) findViewById (R.id.textView2);
-        y = (TextView) findViewById (R.id.textView3);
-        z = (TextView) findViewById (R.id.textView1);
+        currentTime.setText(String.valueOf(System.currentTimeMillis()));
+
         SensorManager sensorManager;
-
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
         sensorManager.registerListener(this, sensorManager.getDefaultSensor
                 (Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
         }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
